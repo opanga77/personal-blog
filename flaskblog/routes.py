88cc70a089756e052a00cafe41922200a4e5c4e5
opanga77 <andersoking77@gmail.com>
2020-05-12 10:17:48 +0300

    commit

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -9,13 +9,13 @@ posts = [
         'author': 'Corey Schafer',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'April 20, 2020'
     },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'April 21, 2020'
     }
 ]
 
